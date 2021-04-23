5c936e53556b25dbae3631837eea4bbe807f6b8e
cain <chen562450669@qq.com>
2021-04-23 22:47:10 +0800

    feat: option devlop -> version and init

--- egg-cli-test-lib/lib/index.js
+++ egg-cli-test-lib/lib/index.js
@@ -3,4 +3,7 @@ module.exports = {
   sum(a, b) {
     return a + b
   },
+  init({option, param}){
+    console.log("执行 init 流程:", option, param);
+  }
 }
--- egg-cli-test/bin/index.js
+++ egg-cli-test/bin/index.js
@@ -1,6 +1,48 @@
 #!/usr/bin/env node
 
-const lib = require('egg-cli-test-lib')
-console.log(lib.sum(1, 2))
+// *----------------------------------------------
 
-console.log('welcome egg-cli-test-ass')
+const lib = require('egg-cli-test-lib')
+const argv = require('process').argv
+
+// *----------------------------------------------
+
+// 执行 cli 的执行命令时，所执行的所有行为：$your-cli command [option] <params>
+// console.log(require('process').argv)
+
+// 0.node 1.your-cli 2~...custom 命令
+const command = argv[2]
+const options = argv.slice(3)
+
+// *----------------------------------------------
+
+// 实现参数解析 init --name
+
+if (options.length > 1) {
+  let [option, param] = options
+
+  option = option.replace('--', '')
+
+  if (command) {
+    if (lib[command]) {
+      lib[command]({option, param})
+    } else {
+      console.log('无效的命令')
+    }
+  } else {
+    console.log('请输入命令')
+  }
+}
+
+// *----------------------------------------------
+
+// 实现参数解析 -- version
+
+// 判断：若参数是以 '--' 或 '-' 开头，则视为全局参数
+if (command.startsWith('--') || command.startsWith('-')) {
+  const globalOption = command.replace(/--|-/g, '')
+  // console.log(globalOption);
+  if (globalOption === 'version' || globalOption === 'V') {
+    console.log('v1.0.3')
+  }
+}
