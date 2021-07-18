d547be1812adc2ecaa149a3291648c0d457bd50d
cain <chen562450669@qq.com>
2021-07-18 13:19:12 +0800

    feat: check Node.js version

--- egg-cli-dev/egg-cli-dev/core/cli/lib/index.js
+++ egg-cli-dev/egg-cli-dev/core/cli/lib/index.js
@@ -8,10 +8,31 @@ module.exports = core
 // .node -> process.dlopen (基本不用)
 // .any  -> .js
 const pkg = require('../package.json')
+const semver = require('semver')
+const colors = require('colors/safe')
+
 const log = require('@egg-cli-dev/log')
+const constant = require('./const') // 环境变量
 
 function core() {
-  checkPkgVersion()
+  try {
+    checkPkgVersion()
+    checkNodeVersion()
+  } catch (e) {
+    log.error(e.message)
+  }
+}
+
+// 检查 node 版本号
+function checkNodeVersion() {
+  const currentVersion = process.version // 获取当前版本号
+  const lowestNodeVersion = constant.LOWEST_NODE_VERSION // 获取最低版本号
+  // 比对校验版本号 -> semver
+  if (!semver.gte(currentVersion, lowestNodeVersion)) {
+    throw new Error(
+      colors.red(`egg-cli 需要安装 v${lowestNodeVersion} 以上版本的 Node.js`),
+    )
+  }
 }
 
 // 检查版本号
