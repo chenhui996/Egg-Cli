51fb98e51b09587bfd4d671f478d46463cfe347a
cain <chen562450669@qq.com>
2021-08-06 14:51:49 +0800

    feat: 判断当前目录是否为空

--- egg-cli-dev/egg-cli-dev/commands/init/lib/index.js
+++ egg-cli-dev/egg-cli-dev/commands/init/lib/index.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const fs = require('fs')
 const Command = require('@egg-cli-dev/command')
 const log = require('@egg-cli-dev/log')
 
@@ -12,7 +13,35 @@ class InitCommand extends Command {
   }
 
   exec() {
-    console.log('init 的业务逻辑')
+    try {
+      // 1. 准备阶段
+      this.prepare()
+      // 2. 下载模版
+      // 3. 安装模版
+    } catch (error) {
+      log.error(error.message)
+    }
+  }
+
+  // 准备阶段
+  prepare() {
+    // 1. 判断当前目录是否为空
+    if (!this.isCwdEmpty()) {
+      // 交互：询问是否继续创建
+    }
+    // 2. 是否启动强制更新
+    // 3. 选择创建项目或组件
+    // 4. 获取项目的基本信息
+  }
+
+  isCwdEmpty() {
+    const localPath = process.cwd()
+    let fileList = fs.readdirSync(localPath)
+    // 文件过滤逻辑 -> 根据需求自行定制
+    fileList = fileList.filter(
+      (file) => !file.startsWith('.') && ['node_modules'].indexOf(file) < 0,
+    )
+    return !fileList || fileList.length <= 0
   }
 }
 
